a84d5854fa5042f7798f217dd4193c50d172d453
MuRye <sm@murye.io>
2017-09-01 06:28:29 +0900

    Update model Facility

--- src/models/Facility.js
+++ src/models/Facility.js
@@ -1,7 +1,14 @@
 const mongoose = require('mongoose')
 
+const Point = mongoose.Schema({
+  type: String,
+  coordinates: Array
+})
+
 const schema = mongoose.Schema({
-  sujCode: { type: Number, required: true }
+  sujCode: { type: Number, required: true },
+  juso: { type: String, required: true },
+  location: { type: Point, required: true }
 })
 
 module.exports = mongoose.model('Facility', schema)
