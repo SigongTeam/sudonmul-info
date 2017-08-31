2c6fa75e8a4e5b4f5823787c6280dbbc7bcb46a8
ChalkPE <chalk@chalk.pe>
2017-08-31 23:29:57 +0900

    Fix eslint errors

--- app/layouts/MainHeader.vue
+++ app/layouts/MainHeader.vue
@@ -1,30 +1,30 @@
 <template>
-	<header class="main-header">
-		<icon icon="information-outline"></icon>
-	</header>
+  <header class="main-header">
+    <icon icon="information-outline"></icon>
+  </header>
 </template>
 
 <style>
-	@import "@css/variables";
-	.main-header {
-		display: flex;
-		align-items: center;
-		padding-left: 10%;
-	  background-color: var(--header-color);
-	  width: 90%;
-	  height: 20vh;
-	  font-family: var(--font);
-	  font-size: 5rem;
-		color: #fff;
-	}
+  @import "@css/variables";
+  .main-header {
+    display: flex;
+    align-items: center;
+    padding-left: 10%;
+    background-color: var(--header-color);
+    width: 90%;
+    height: 20vh;
+    font-family: var(--font);
+    font-size: 5rem;
+    color: #fff;
+  }
 </style>
 
 <script>
 import Icon from '../components/Icon.vue'
 
 export default {
-	  components: {
-	    Icon
-	  }
+  components: {
+    Icon
+  }
 }
 </script>
