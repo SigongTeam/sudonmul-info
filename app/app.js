243e21839beefd76e141c7a86e64d2b610d25502
ChalkPE <chalk@chalk.pe>
2017-09-01 12:00:58 +0900

    Remove unused dependencies

--- app/app.js
+++ app/app.js
@@ -2,10 +2,7 @@ import 'whatwg-fetch'
 import 'babel-polyfill'
 
 import Vue from 'vue'
-import Vuex from 'vuex'
 import App from './App.vue'
-
-Vue.use(Vuex)
 
 // eslint-disable-next-line no-new
 new Vue({
