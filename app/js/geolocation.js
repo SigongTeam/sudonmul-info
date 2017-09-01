41b6e68b0b4296a8dc210a961105479e3bf18c85
ChalkPE <chalk@chalk.pe>
2017-09-01 14:20:34 +0900

    Add sweetalert

--- app/js/geolocation.js
+++ app/js/geolocation.js
@@ -1,7 +1,14 @@
+import sweetalert from 'sweetalert'
+
 export default class Geolocation {
   static async getParsedPosition () {
-    const position = await this.getCurrentPosition()
-    return this.parsePosition(position)
+    try {
+      const pos = await this.getCurrentPosition()
+      return this.parsePosition(pos)
+    } catch (err) {
+      sweetalert('이런...', '현재 위치 정보를 불러올 수 없습니다.\n기본값으로 서울시청을 불러옵니다.', 'error')
+      return { latitude: 37.5222067, longitude: 126.9375121 }
+    }
   }
 
   static getCurrentPosition () {
