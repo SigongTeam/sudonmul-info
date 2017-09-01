b2964c1c9e052897c65d9b75792ebf7c97d4b5a1
Khinenw <deu07115@gmail.com>
2017-09-01 17:03:16 +0900

    Updated chart section

--- app/layouts/InfoSection.vue
+++ app/layouts/InfoSection.vue
@@ -1,14 +1,6 @@
 <template>
-  <section class="info-section">
-    <carousel>
-      <slide>
-        <h1>Coin</h1>
-        <div>
-          300mL 마실 때마다 약 n원이 절약됩니다. (다나와 기준 최소량)
-        </div>
-      </slide>
-    </carousel>
-  </section>
+  <tap-section class="info-section">
+  </tap-section>
 </template>
 
 <style>
@@ -16,9 +8,5 @@
 </style>
 
 <script>
-import { Carousel, Slide } from 'vue-carousel'
-
-export default {
-  components: { Carousel, Slide }
-}
+export default {}
 </script>
