52cdf80a78b0bfc5da7c4469d3d7077ae05e52e2
ChalkPE <chalk@chalk.pe>
2017-09-01 17:02:33 +0900

    Danawa integration

--- app/App.vue
+++ app/App.vue
@@ -4,6 +4,7 @@
     <location-section></location-section>
     <write-section @write="refresh"></write-section>
     <review-section ref="review"></review-section>
+    <info-section></info-section>
   </main>
 </template>
 
@@ -20,13 +21,15 @@ import LocationSection from './layouts/LocationSection.vue'
 import MainHeader from './layouts/MainHeader.vue'
 import ReviewSection from './layouts/ReviewSection.vue'
 import WriteSection from './layouts/WriteSection.vue'
+import InfoSection from './layouts/InfoSection.vue'
 
 export default {
   components: {
     LocationSection,
     MainHeader,
     ReviewSection,
-    WriteSection
+    WriteSection,
+    InfoSection
   },
 
   methods: {
--- app/layouts/InfoSection.vue
+++ app/layouts/InfoSection.vue
@@ -1,7 +1,8 @@
 <template>
   <section class="info-section">
-    <h1>Coin</h1>
+    <h1>{{ averagePrice }}원</h1>
     <div>
+
       300mL 마실 때마다 약 n원이 절약됩니다. (다나와 기준 최소량)
     </div>
   </section>
@@ -12,6 +13,22 @@
 </style>
 
 <script>
+import axios from 'axios'
+import sweetalert from 'sweetalert'
+
 export default {
+  data: () => ({ danawa: [] }),
+
+  computed: {
+    averagePrice () {
+      return this.danawa.reduce((a, b) => a + b, 0) / (this.danawa.length || 1)
+    }
+  },
+
+  created () {
+    axios.get('/danawa')
+      .then(({ data }) => (this.danawa = data))
+      .catch(() => sweetalert('이런...', '다나와 물가를 찾을 수 없습니다.', 'error'))
+  }
 }
 </script>
