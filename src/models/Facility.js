e025f5080940c938e4a9b20fde82347c53f69783
a1p4ca <sm@murye.io>
2017-09-01 15:42:11 +0900

    Insert facilities written

--- src/models/Facility.js
+++ src/models/Facility.js
@@ -1,13 +1,18 @@
 const mongoose = require('mongoose')
 
-const schema = mongoose.Schema({
-  sujCode: { type: Number, required: true },
-  juso: { type: String, required: true },
-  location: { type: String, coordinates: [Number], required: true },
-  updated: { type: Date, required: true },
+const Quality = mongoose.Schema({
+  date: { type: Date, required: true },
   clVal: { type: Number, required: true },
   phVal: { type: Number, required: true },
   tbVal: { type: Number, required: true }
+})
+
+const schema = mongoose.Schema({
+  name: { type: String, required: true },
+  number: { type: Number, required: true },
+  juso: { type: String, required: true },
+  location: { type: String, coordinates: [Number], required: true },
+  qualities: { type: [Quality], required: true }
 })
 
 module.exports = mongoose.model('Facility', schema)
