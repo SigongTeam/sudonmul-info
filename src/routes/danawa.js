ed0a31ade7201c61a082852650a59a29b78aeaea
ChalkPE <chalk@chalk.pe>
2017-09-02 00:11:41 +0900

    Danawa caching

--- src/routes/danawa.js
+++ src/routes/danawa.js
@@ -3,29 +3,48 @@ const Router = require('koa-router')
 const { launch } = require('chrome-launcher')
 const CDP = require('chrome-remote-interface')
 
-const chromeFlags = ['--disable-gpu', '--headless']
-const startingUrl = 'http://search.danawa.com/dsearch.php?k1=%EC%83%9D%EC%88%98+2L&module=goods&act=dispMain'
+const cache = {
+  value: null,
+  timestamp: new Date(0),
+  period: 1000 * 60 * 60 * 3 // 3h
+}
+
+const launchOptions = {
+  chromeFlags: ['--disable-gpu', '--headless'],
+  startingUrl: 'http://search.danawa.com/dsearch.php?k1=%EC%83%9D%EC%88%98+2L&module=goods&act=dispMain'
+}
+
 const expression = `Array.prototype.slice
-    .call(document.querySelectorAll('.prod_pricelist ul li:first-of-type .memory_price_sect'))
-    .map(price => parseInt(price.textContent.split('/')[0].replace(/\\D/g, '')))`
+  .call(document.querySelectorAll('.prod_pricelist ul li:first-of-type .memory_price_sect'))
+  .map(price => parseInt(price.textContent.split('/')[0].replace(/\\D/g, '')))`
 
-const delay = (func, time) =>
-  new Promise((resolve, reject) =>
-    setTimeout(() => func().then(resolve), time))
+function sleep (time) {
+  return new Promise((resolve, reject) => setTimeout(resolve, time))
+}
 
-module.exports = new Router({ prefix: '/danawa' }).get('/', async ctx => {
-  const chrome = await launch({ startingUrl, chromeFlags })
+async function gather () {
+  const chrome = await launch(launchOptions)
   const protocol = await CDP({ port: chrome.port })
 
   const { Page, Runtime } = protocol
   await Promise.all([Page.enable(), Runtime.enable()])
   await Page.loadEventFired()
 
-  await delay(async () => {
-    const res = await Runtime.evaluate({ expression, returnByValue: true })
-    ctx.body = res.result.value
+  await sleep(5000)
+  const res = await Runtime.evaluate({ expression, returnByValue: true })
 
-    protocol.close()
-    chrome.kill()
-  }, 5000)
-})
+  protocol.close()
+  chrome.kill()
+
+  return res.result.value
+}
+
+module.exports = new Router({ prefix: '/danawa' })
+  .get('/', async ctx => {
+    if (!cache.value || (new Date() - cache.timestamp) >= cache.period) {
+      cache.value = await gather()
+      cache.timestamp = new Date()
+    }
+
+    ctx.body = cache.value
+  })
