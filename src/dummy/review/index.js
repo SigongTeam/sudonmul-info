ce83846997ce5b0c01d4c8fda4b49127d43789db
ChalkPE <chalk@chalk.pe>
2017-09-01 12:32:48 +0900

    Fix review error

--- src/dummy/review/index.js
+++ src/dummy/review/index.js
@@ -2,18 +2,27 @@ const count = 2
 const Review = require('../../models/Review')
 
 const ips = require('./ips')
+const jusos = require('./jusos')
 const ratings = require('./ratings')
 const messages = require('./messages')
-const locations = require('./locations')
 const timestamps = require('./timestamps')
 
-const review = () => new Review({
-  ip: ips(),
-  rating: ratings(),
-  message: messages(),
-  location: locations(),
-  timestamp: timestamps()
-})
+const { geocoder } = require('../../utils')
+
+const review = () => {
+  const juso = jusos()
+  const location = geocoder.getDByJuso(juso)
+
+  return new Review({
+    ip: ips(),
+    rating: ratings(),
+    message: messages(),
+    timestamp: timestamps(),
+
+    juso,
+    location: { type: 'Point', coordinates: location }
+  })
+}
 
 module.exports = () => Promise
   .all([...Array(count)].map(review).map(r => r.save()))
