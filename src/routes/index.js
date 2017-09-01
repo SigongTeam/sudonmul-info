b3975a2e6af2e43ee39b6959121af647085e9266
ChalkPE <chalk@chalk.pe>
2017-09-01 13:45:05 +0900

    Add where router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 const compose = require('koa-compose')
 
-const routeNames = ['review']
+const routeNames = ['review', 'where']
 const routes = routeNames
   .map(n => require(`./${n}`))
   .map(r => [r.routes(), r.allowedMethods()])
