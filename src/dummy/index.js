9b8d616f9cd8f2056e6edd2357c0cb7e84794b1c
ChalkPE <chalk@chalk.pe>
2017-09-01 21:51:46 +0900

    Remove dummy/facility

--- src/dummy/index.js
+++ src/dummy/index.js
@@ -1,26 +1,25 @@
 const count = 2
-const Review = require('../../models/Review')
+const Review = require('../models/Review')
 
 const ips = require('./ips')
 const jusos = require('./jusos')
 const ratings = require('./ratings')
 const messages = require('./messages')
 const timestamps = require('./timestamps')
-
-const { geocoder } = require('../../utils')
+const { geocoder } = require('../utils')
 
 const review = async () => {
   const juso = jusos()
-  const location = await geocoder.getDByJuso(juso)
+  const dCode = await geocoder.getDByJuso(juso)
+  const location = { type: 'Point', coordinates: dCode }
 
   const review = new Review({
+    juso,
+    location,
     ip: ips(),
     rating: ratings(),
     message: messages(),
-    timestamp: timestamps(),
-
-    juso,
-    location: { type: 'Point', coordinates: location }
+    timestamp: timestamps()
   })
 
   return review.save()
--- src/dummy/jusos.js
+++ src/dummy/jusos.js
@@ -1,4 +1,4 @@
-const locations = [
+const jusos = [
   '강원도 홍천군 내촌면 물걸리',
   '서울특별시 강동구 암사동',
   '대한민국 경기도 부천시 오정구',
@@ -9,4 +9,4 @@ const locations = [
   '충청남도 금산군 남이면'
 ]
 
-module.exports = () => locations[Math.floor(Math.random() * locations.length)]
+module.exports = () => jusos[Math.floor(Math.random() * jusos.length)]
